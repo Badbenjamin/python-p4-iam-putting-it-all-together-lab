5fa8b739cfe4dce60b190f60a9c7ed5db725ae75
ben mccarthy <bmccarthy90@gmail.com>
2024-09-24 16:52:50 -0400

    some tests passing, needs work

--- server/app.py
+++ server/app.py
@@ -7,26 +7,75 @@ from sqlalchemy.exc import IntegrityError
 from config import app, db, api
 from models import User, Recipe
 
-class Signup(Resource):
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    if data.get('username') != None:
+        new_user = User(
+            username = data.get('username'),
+            image_url = data.get('image_url'),
+            bio = data.get('bio')
+        )
+        new_user.password_hash=data['password']
+        
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
+        print(session)
+        return new_user.to_dict(), 201
+    else:
+        return {'message' : 'error'}, 422
+
+@app.route('/check_session')
+def check_session():
+    #  query db for user. make sure user cookie id matches user id in db
+    print(session)
+    user = User.query.filter(User.id == session['user_id']).first()
+    print(user)
+    if user is None:
+
+        return {"error": 'not logged in'}, 401
+    else:
+        return user.to_dict(), 200
+   
+
+@app.route('/login', methods=['POST'])
+def login():
+    
+    data = request.get_json()
+    print("d", data)
+    print("un", data.get('username'))
+    user = User.query.filter(User.username == data.get('username')).first()
+    print("u", user)
+    if user is None:
+        return {'error' : 'login failed'}, 401
+    if not user.authenticate(data.get('password')):
+        return {'error' : 'login failed'}, 401
+    
+    session['user_id']=user.id
+
+    return user.to_dict(), 200
+
+
+@app.route('/logout', methods=['DELETE'])
+def logout():
+    print(session)
+    if session['user_id'] is not None:
+        session['user_id'] = None
+        return {}, 204
+    else:
+        return {'error' : 'not logged in'}, 401
+
+
+@app.route('/recipes')
+def get_recipes():
     pass
 
-class CheckSession(Resource):
-    pass
-
-class Login(Resource):
-    pass
-
-class Logout(Resource):
-    pass
-
-class RecipeIndex(Resource):
-    pass
-
-api.add_resource(Signup, '/signup', endpoint='signup')
-api.add_resource(CheckSession, '/check_session', endpoint='check_session')
-api.add_resource(Login, '/login', endpoint='login')
-api.add_resource(Logout, '/logout', endpoint='logout')
-api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+# api.add_resource(Signup, '/signup', endpoint='signup')
+# api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+# api.add_resource(Login, '/login', endpoint='login')
+# api.add_resource(Logout, '/logout', endpoint='logout')
+# api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,9 +7,53 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    # in this relationship, Recipe class backpops user relationship in Recipe class
+    recipes = db.relationship('Recipe', back_populates='user')
+
+    # function in hybrid property is named for method you use to set pw
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError
+    
+    @password_hash.setter
+    def password_hash(self, plain_text_password):
+        bytes = plain_text_password.encode('utf-8')
+        self._password_hash = bcrypt.generate_password_hash(bytes)
+
+    def authenticate(self, password):
+         return bcrypt.check_password_hash(
+              self._password_hash,
+              password.encode('utf-8')
+         )
+         
+
+    def __repr__(self):
+         return f'<User {self.id}, {self.username}, {self._password_hash}>'
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # in this relationship, User class backpops recipies relationship in User class
+    user = db.relationship('User', back_populates='recipes')
+
+    @validates('instructions')
+    def validate_instructions(self, key, new_instructions):
+            if len(new_instructions) < 50:
+                raise ValueError
+            else:
+                 return new_instructions
+
+    def __repr__(self):
+         return f'<User {self.id}, {self.title}>'
